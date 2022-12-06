8d20f971ea4b89151d366b0fa5f69290fd3a3415
Noah Ham <spiderclone7@gmail.com>
2022-12-06 14:09:36 -0500

    Start to the Home page

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <div id="header">
+      <h1 id="title">Glider</h1>
+      <img id="glider-img" src="..\assets\Glider-1.png" alt="Glider Image" >
+    </div>
+    <div id="filter">
+      <h2 id="filter-names">Example</h2>
+    </div>
+    <div id="main">
+      <h3 id="example">Another</h3>
+    </div>
   </div>
 </template>
 
@@ -10,3 +18,42 @@ export default {
   name: "home"
 };
 </script>
+
+<style>
+
+body {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+
+div.home {
+  display: grid;
+  grid-template-areas: 
+  "header"
+  "filter"
+  "main";
+  grid-template-rows: .1fr .1fr 1fr;
+}
+
+#title {
+  display: inline-block;
+}
+
+#glider-img {
+  height: 10%;
+}
+
+#header {
+  grid-area: header;
+}
+
+#filter {
+  grid-area: filter;
+}
+
+#main {
+  grid-area: main;
+}
+
+</style>
