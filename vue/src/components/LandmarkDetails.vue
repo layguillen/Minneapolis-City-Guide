8d856ca0412b27e675a6f8963de56eefe95d73ca
Mohamad Sareini <mosareini@gmail.com>
2022-12-07 20:48:38 -0500

    Landmark Details - Mo

--- vue/src/components/LandmarkDetails.vue
+++ vue/src/components/LandmarkDetails.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container">
+    <!-- image -->
      <div class="griditem1">
-       <img src="../assets/Landmark1.png" alt="">
+       <img id = "landmark1" src="../assets/Landmark1.png" alt="">
      </div>
+     <!-- name -->
      <div class="griditem2">
        <h1>{{this.$store.state.currentLandmark.name}}</h1>
      </div>
+     <!-- description-->
      <div class="griditem3">
-       <p>{{this.$store.state.currentLandmark.description}}</p>
+      <p id = "description">{{this.$store.state.currentLandmark.description}}</p>
      </div>
   </div>
 </template>
@@ -39,19 +42,52 @@ export default {
 .container{
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: repeat(3, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  background-color: #004E64
 }
 .griditem1{
   
 }
+/*
+@font-face {
+  font-family: Montserrat_Alternates;
+  src: url(montserrat_alternate;);
+}*/ 
 .griditem2{
-  grid-column-start: 4;
-  grid-column-end: 5;
+  grid-column-start: 2;
+  grid-column-end: 6;
+  grid-row-start: 1;
+  grid-row-end:2;
+  text-align:center;
+  margin-top: 20px;
+  font-family: fantasy;
+  font-size: 200%;
+  color:whitesmoke;
+  text-decoration-line: underline;
+  text-decoration-color: whitesmoke;
+  text-underline-offset: 20px ;
+  
 }
 .griditem3{
-  grid-column-start: 1;
-  grid-column-end: 2;
+  grid-column-start:2;
+  grid-column-end: 6;
+  grid-row-start: 1;
+  margin-left:25px;
+  font-family:fantasy;
+  font-size: 30px;
+  color: whitesmoke;
+  margin-top: 150px;
+}
 
+#description{
+}
+
+#landmark1 {
+  width: 700px;
+  height: 400px;
+  margin-top: 60px;
+  margin-left: 70px;
+  float: left;
 }
 
 </style>
