4916fe0d9a354e854ee4165c37dcca3de98692ad
Layla Guillen <laylazahniser@gmail.com>
2022-12-09 09:27:21 -0600

    added to Itinerary page

--- vue/src/components/ItineraryLandmarksLister.vue
+++ vue/src/components/ItineraryLandmarksLister.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="itinerary-landmarks">
+      <h1>Landmarks</h1>
       <div id="landmarks" v-for="landmark in this.$store.state.itineraryLandmarks" v-bind:key="landmark.id">
           <img id="landmark-img" src="../assets/Landmark2.png" alt="Landmark Img">
           <h2 id="landmark-name">{{ landmark.name }}</h2>
@@ -11,7 +12,7 @@
 export default {
     name: "itinerary-landmarks-lister",
     created() {
-        this.$store.state.landmarks.array.forEach(element => {
+        this.$store.state.landmarks.forEach(element => {
             this.$store.commit('SET_ITINERARY_LANDMARK', element);
         });
     },
@@ -22,5 +23,11 @@ export default {
 </script>
 
 <style>
+
+h1{
+    text-align: center;
+    color: #004E64;
+    font-family: 'Montserrat Alternates', 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif';
+}
 
 </style>
