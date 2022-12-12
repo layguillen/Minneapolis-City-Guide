243b5ccfe846910213508dd38ee3da32b4264cd5
Christopher Luna <christtopherluna@gmail.com>
2022-12-12 17:31:27 -0500

    Test bodies

---
+++ java/src/test/java/com/techelevator/dao/JdbcAddressDaoTest.java
@@ -0,0 +1,25 @@
+package com.techelevator.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JdbcAddressDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listOfAddresses() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void createAddress() {
+    }
+}
